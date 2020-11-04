4c5af3cd54147d7980abb11b5a34074134552eab
SRBVT <63731106+SRBVT@users.noreply.github.com>
2020-11-04 07:53:11 -0500

    VR Viewfinder Project

---
+++ VR Viewfinder Full Project/Assets/Scripts/PointerInput.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PointerInput : MonoBehaviour
+{
+    public Camera eventCamera;
+
+    public int layerMask;
+    public float maxDistance;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //Debug.Log(Input.mousePosition);
+        //Debug.Log(eventCamera);
+        Ray ray = eventCamera.ScreenPointToRay(new Vector3(eventCamera.pixelWidth / 2, eventCamera.pixelHeight / 2, 0));
+
+        //Ray ray = eventCamera.ScreenPointToRay(new Vector3(0, 0, 0));
+        Debug.DrawRay(ray.origin, ray.direction * 10, Color.red);
+        if (Physics.Raycast(ray, maxDistance, layerMask)){
+            Debug.Log("Origin: " + ray.origin);
+            Debug.Log("Direction: "+ ray.direction);
+        }  
+    }
+}
